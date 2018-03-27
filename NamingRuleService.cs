5325e9f39f49762ba8e45fa812a494fa9ea2149c
FrankSunInCreatidea <37104962+FrankSunInCreatidea@users.noreply.github.com>
2018-03-27 11:38:49 +0800

    Add files via upload

---
+++ NamingRuleService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingtungCity.Culture.IMS.Service.Logic
+{
+    public class NamingRuleService
+    {
+        public int TestFunction(int index)
+        {
+            switch (index)// bad
+            {
+                case 1: break;
+                default: break;
+            }
+
+            switch (index)// good
+            {
+                case 1:
+                    break;
+                default:
+                    break;
+            }
+
+            // bad
+            if (index == 0) DoTrue();
+            else
+                DoFalse();
+
+            // good 即使陳述區塊內僅有一行程式，也請使用大括號圈住，此規範包含if -else, for, foreach, while, do -while
+            if (index == 0)
+            {
+                DoTrue();
+            }
+            else
+            {
+                DoFalse();
+            }
+
+            if (DoTrue() == true) { DoTrue(); }// bad
+            if (DoTrue()) { DoTrue(); }// good 在條件陳述式中，不將布林變數與true/false作比較
+
+            return (index * (index + 1) / 2);// bad
+            return index * (index + 1) / 2;// good 回傳值本身不再使用括號
+        }
+
+        private bool DoTrue()
+        {
+            return true;
+        }
+        private bool DoFalse()
+        {
+            return false;
+        }
+
+        public void TestStringFunction(string value)
+        {
+            string NewString = "ABCDEFG"; //新字串 // bad
+            string newString = "ABCDEFG"; // 新字串 // good 註解開始前先空一格，私有變數(Local Variable)使用 camelCasting 命名法(第一個單字字首小寫，其他單字字首大寫)
+
+            if (value != null | value != "") value = NewString.Replace("A", ",").Replace("B", "_").Replace("C", " ");// bad
+
+            if (!string.IsNullOrWhiteSpace(value))// good
+            {
+                value = newString.Replace("A", ",").Replace("B", "_").Replace("C", " ");
+            }
+        }
+
+    }
+}
